584b48b9ae66961934d96f0c4ace2bed3ddeb26c
Imhvfd <tanverkaay@gmail.com>
2024-10-30 09:34:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==wcNRzRFdkMtMjVf12UXVDTrFWMR5kVZBDahV2ZSdGe4NHZ5B1SSp1ZTdHdjIkaUJVawEENpwd=123588770329',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
